bc9c33d60833407477ccfca1d786774ba4e62da8
Anbalagan <anbumannan93@gmail.com>
2021-06-21 16:14:07 +0530

    Add files via upload
    Re-Uploaded With Solution

---
+++ ShopBridgeService/ShopBridgeService/IShopBridgeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ShopBridgeService
+{
+    [ServiceContract]
+    public interface IShopBridgeService
+    {
+
+        [OperationContract(Name = "ProcessData")]
+        [WebInvoke(Method = "POST", BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "/ProcessData")]
+        Stream ProcessData(Stream InputData);
+
+    }
+}
