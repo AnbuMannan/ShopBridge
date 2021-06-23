019c1a751028d4a2df4a982faefda3ca05306580
Anbalagan <anbumannan93@gmail.com>
2021-06-23 16:36:50 +0530

    Add files via upload

---
+++ ShopBridgeServiceWebAPI/ShopBridgeService/Controllers/CreateItemsController.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ShopBridgeService.Controllers
+{
+    public class CreateItemsController : ApiController
+    {
+        public string Post([FromBody]XElement Input)
+        {
+            string Output="";
+
+            Output = Helper.ProcessData(Input, "CreateItem") ;
+
+            return Output;
+
+        }
+    }
+}
---
+++ ShopBridgeServiceWebAPI/ShopBridgeService/Controllers/UpdateItemsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Xml.Linq;
+
+namespace ShopBridgeService.Controllers
+{
+    public class UpdateItemsController : ApiController
+    {
+        public string Post([FromBody]XElement Input)
+        {
+            string Output = "";
+
+            Output = Helper.ProcessData(Input, "UpdateItem");
+
+            return Output;
+
+        }
+    }
+}
